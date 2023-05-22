0be129fd6d1e15da19ca15d3ee9f0b131a443f61
sheriff ibrahim <sheezey@UNBEKNOWNST-MacBook-Air.local>
2023-05-22 11:44:54 +0100

    changed about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -8,29 +8,44 @@ export default function About() {
                <div className="col-12 col-lg-6">
                   <div className="abt-text">
                      <div className="head">
-                        <span>About <b>GENIE</b></span>
+                        <span>
+                           About <b>GENIE</b>
+                        </span>
                      </div>
                      <div className="text">
                         <p>
-                           Lorem ipsum dolor sit amet consectetur adipisicing
-                           elit. Sit, molestiae sed dolorem officiis ipsam
-                           maiores, reprehenderit quod, error necessitatibus
-                           laborum libero sapiente? Reiciendis corrupti commodi
-                           tenetur placeat consectetur aspernatur eaque.
+                           Once upon a time, in a distant land, there was a
+                           mystical being named Genie. For centuries, Genie
+                           roamed the realms, spreading joy and granting wishes
+                           to those in need. But as the world evolved, Genie
+                           felt the need to adapt and embrace the digital age.
+                           Now, Genie stands tall in the crypto realm, ready to
+                           unleash his magic, prosperity and abundance to those
+                           who believe in his power.
                         </p>
-                        <a href="#" className="btn btn-sm">See Chart</a>
+                        <a href="#" className="btn btn-sm">
+                           See Chart
+                        </a>
                      </div>
                   </div>
                </div>
                <div className="col-12 col-lg-6">
-                <div className="abt-img d-flex justify-content-center">
-                    <img src="gg.png" alt="About Genie" className="img-fluid" />
-                </div>
+                  <div className="abt-img d-flex justify-content-center">
+                     <img
+                        src="gg.png"
+                        alt="About Genie"
+                        className="img-fluid"
+                     />
+                  </div>
                </div>
             </div>
          </div>
-         <div className="bg-img bg1"><img src="gw.png"/></div>
-         <div className="bg-img"><img src="gw.png"/></div>
+         <div className="bg-img bg1">
+            <img src="gw.png" />
+         </div>
+         <div className="bg-img">
+            <img src="gw.png" />
+         </div>
       </div>
    );
 }
